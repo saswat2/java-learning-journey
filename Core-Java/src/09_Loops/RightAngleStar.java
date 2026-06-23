e882f676fe503b3110084da175dc5c4680191e0d
saswat2 <saswatmohapatra979@gmail.com>
2026-06-23 19:26:30 +0530

    Added RightAngleStar program

---
+++ Core-Java/src/09_Loops/RightAngleStar.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class RightAngleStar {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number of rows");
+        int n = scan.nextInt();
+        printRightAngleStar(n);
+        scan.close();
+    }
+
+    private static void printRightAngleStar(int n) {
+        // row loop
+        for (int i = 1; i <= n ; i++){
+            //stars loop
+            for (int j = 1; j<= i ; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
