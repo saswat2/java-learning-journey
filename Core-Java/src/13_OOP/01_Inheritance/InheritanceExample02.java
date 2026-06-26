ec6df81bfe41b9dd04e70a900c5b37a67443a5a6
saswat2 <saswatmohapatra979@gmail.com>
2026-06-26 16:02:19 +0530

    Added InheritanceExample02 program

---
+++ Core-Java/src/13_OOP/01_Inheritance/InheritanceExample02.java
@@ -0,0 +1,32 @@
+class person {
+    String name = "Saswat";
+    int age = 23;
+
+    void displayPerson(){
+        System.out.println("Name : "+name);
+        System.out.println("Age : "+age);
+    }
+}
+// Chlid class
+class Emp extends person{
+    int emId = 101;
+    double salary = 50000;
+
+    void displayEmp(){
+        System.out.println("Employee ID : "+emId);
+        System.out.println("Salary : "+salary);
+
+    }
+}
+public class InheritanceExample02 {
+    public static void main(String[] args){
+        Emp emp = new Emp();
+
+        // parent class method
+        emp.displayPerson();
+
+        //Child class method
+        emp.displayEmp();
+
+    }
+}
